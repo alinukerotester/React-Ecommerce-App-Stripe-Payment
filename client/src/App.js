1d343b12d059552b54caf8b3126a0106f8a2265d
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2023-01-20 22:44:24 +0200

    more data

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import {
 } from "react-router-dom";
 import Home from "./scenes/home/Home";
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -22,10 +25,13 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
