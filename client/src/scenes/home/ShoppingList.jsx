612b3d41b567f79a1b596217539eccd777041e4b
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2023-01-23 21:12:12 +0200

    all imports for this file

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -1,4 +1,17 @@
+import React, { useEffect, useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import Item from "../../components/Item";
+import { Typography } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useDispatch, useSelector } from "react-redux";
+import { setItems } from "../../state";
 
-const ShoppingList = () => { }
+const ShoppingList = () => {
+    const dispatch = useDispatch();
+    const [value, setValue] = useState('all');
+    const items = useSelector()
+}
 
 export default ShoppingList;
